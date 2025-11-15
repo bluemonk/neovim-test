7a387d6b31fdbbc82dda8678eed6a513a7e53daa
Marco Ceresa <bluemonk@users.noreply.github.com>
2025-11-15 09:54:23 +0100

    Fixed typo

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Neovim flake test";
-  inputs = nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
