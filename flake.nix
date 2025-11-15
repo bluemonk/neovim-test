f6662bc69b7a5855553c4c5c5b0b760adf09e499
Marco Ceresa <bluemonk@users.noreply.github.com>
2025-11-15 09:37:52 +0100

    Further tidy up

--- flake.nix
+++ flake.nix
@@ -1,16 +1,10 @@
 {
   description = "Neovim flake test";
+  inputs = nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-  };
-
-  outputs =
-    { self, nixpkgs }:
+  outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
 
       neovim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
         luaRcContent = ''
@@ -19,10 +13,6 @@
       };
     in
     {
-
       packages.x86_64-linux.neovim = neovim;
-
-      packages.x86_64-linux.default = self.packages.x86_64-linux.neovim;
-
     };
 }
